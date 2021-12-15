761c92830397b9b3b5a24f821e8136d81a904f30
NidhiSri1 <srinidhivaja@gmail.com>
2021-12-15 13:42:32 +0530

    updated js

--- product.js
+++ product.js
@@ -7,9 +7,9 @@ var dataOfProduct = [
   {
     proudimg:
       "https://lmsin.net/cdn-cgi/image/h=493,w=333,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010601390-Pink-Magenta-1000010601390-10132021_01-2100.jpg",
-    produPrice: "599",
+    produPrice: "₹599",
     prodDesc: "MAX Textured Round Neck Sweatshirt",
-    productcolor: "black",
+    productcolor: "pink",
     producttype: "Pullover",
     productdesgine: "solid",
     productSleveLength: "Full Sleves",
@@ -17,16 +17,98 @@ var dataOfProduct = [
     productpromotions: "Max300",
     productDiscount: "40% above",
   },
+  {
+    proudimg:
+      "https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010756375-Blue-Malibu-1000010756375-11112021_01-2100.jpg",
+    produPrice: "₹999",
+    prodDesc: "MAX Solid Hooded Sweatshirt",
+    productcolor: "blue",
+    producttype: "Hoddie",
+    productdesgine: "solid",
+    productSleveLength: "Full Sleves",
+    Browse: "New",
+    productpromotions: "Max300",
+    productDiscount: "10% above",
+  },
+  {
+    proudimg:
+      "https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010682861-Pink-Candypink-1000010682861-9222021_01-2100.jpg",
+    produPrice: "₹499",
+    prodDesc: "MAX Solid Full Sleeves Sweatshirt",
+    productcolor: "pink",
+    producttype: "Hoddie",
+    productdesgine: "solid",
+    productSleveLength: "Full Sleves",
+    Browse: "New",
+    productpromotions: "Sale",
+    productDiscount: "40% above",
+  },
+  {
+    proudimg:
+      "https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010742415-Blue-Blue-1000010742415-11042021_01-2100.jpg",
+    produPrice: "₹799",
+    prodDesc: "MAX Textured Ribbed Sweatshirt",
+    productcolor: "blue",
+    producttype: "Hoddie",
+    productdesgine: "color blocked",
+    productSleveLength: "Full Sleves",
+    Browse: "New",
+    productpromotions: "Sale",
+    productDiscount: "40% above",
+  },
+  {
+    proudimg:
+      "https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010410362-Purple-Mauve-1000010410362-7292021_01-2100.jpg",
+    produPrice: "₹999",
+    prodDesc: "MAX Textured Hooded Sweatshirt",
+    productcolor: "pink",
+    producttype: "Hoddie",
+    productdesgine: "solid",
+    productSleveLength: "Full Sleves",
+    Browse: "Online Only",
+    productpromotions: "Sale",
+    productDiscount: "60% above",
+  },
+  {
+    proudimg:
+      "https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010544891-Purple-Lavender-1000010544891-9292021_01-2100.jpg",
+    produPrice: "₹699",
+    prodDesc: "MAX Textured Mock Collar Sweatshirt",
+    productcolor: "blue",
+    producttype: "Hoddie",
+    productdesgine: "textured",
+    productSleveLength: "Full Sleves",
+    Browse: "Online Only",
+    productpromotions: "Sale",
+    productDiscount: "60% above",
+  },
+  {
+    proudimg:
+      "https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010541877-Pink-Blush-1000010541877-9152021_01-2100.jpg",
+    produPrice: "₹599",
+    prodDesc: "MAX Printed Round Neck Fit Sweatshirt",
+    productcolor: "pink",
+    producttype: "Hoddie",
+    productdesgine: "printed",
+    productSleveLength: "Full Sleves",
+    Browse: "Online Only",
+    productpromotions: "Max300",
+    productDiscount: "60% above",
+  },
 ];
+var count = document.querySelector("#countProduct");
+var ProductTotal = dataOfProduct.length;
+count.textContent = ProductTotal + " " + "Products";
 
 dataOfProduct.map(function (ele) {
+  var appenddiv = document.querySelector("#allProduct");
   var productDiv = document.createElement("div");
   productDiv.setAttribute("id", "productDivSpecific");
 
   var productImg = document.createElement("img");
-  productImg.setAttribute("src", "ele.proudimg");
+  productImg.setAttribute("src", ele.proudimg);
 
-  var proudprice = document.createElement("h3");
+  var proudprice = document.createElement("h2");
   proudprice.textContent = ele.produPrice;
 
   var prouddescription = document.createElement("p");
@@ -34,6 +116,7 @@ dataOfProduct.map(function (ele) {
   prouddescription.textContent = ele.prodDesc;
 
   productDiv.append(productImg, proudprice, prouddescription);
+  appenddiv.append(productDiv);
 });
 
 //  FOOTER SECTION
