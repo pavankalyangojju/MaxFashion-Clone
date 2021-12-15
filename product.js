d26217b0769ab32f15722de486ceffc573bfefe1
NidhiSri1 <srinidhivaja@gmail.com>
2021-12-15 17:29:14 +0530

    changes

--- product.js
+++ product.js
@@ -15,7 +15,7 @@ var dataOfProduct = [
     productSleveLength: "Full Sleves",
     Browse: "New",
     productpromotions: "Max300",
-    productDiscount: "40% above",
+    productDiscount: "40",
   },
   {
     proudimg:
@@ -28,12 +28,12 @@ var dataOfProduct = [
     productSleveLength: "Full Sleves",
     Browse: "New",
     productpromotions: "Max300",
-    productDiscount: "10% above",
+    productDiscount: "10",
   },
   {
     proudimg:
       "https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010682861-Pink-Candypink-1000010682861-9222021_01-2100.jpg",
-    produPrice: "₹499",
+    produPrice: "499",
     prodDesc: "MAX Solid Full Sleeves Sweatshirt",
     productcolor: "pink",
     producttype: "Hoddie",
@@ -41,7 +41,7 @@ var dataOfProduct = [
     productSleveLength: "Full Sleves",
     Browse: "New",
     productpromotions: "Sale",
-    productDiscount: "40% above",
+    productDiscount: "40",
   },
   {
     proudimg:
@@ -54,7 +54,7 @@ var dataOfProduct = [
     productSleveLength: "Full Sleves",
     Browse: "New",
     productpromotions: "Sale",
-    productDiscount: "40% above",
+    productDiscount: "40",
   },
   {
     proudimg:
@@ -67,7 +67,7 @@ var dataOfProduct = [
     productSleveLength: "Full Sleves",
     Browse: "Online Only",
     productpromotions: "Sale",
-    productDiscount: "60% above",
+    productDiscount: "60",
   },
   {
     proudimg:
@@ -80,7 +80,7 @@ var dataOfProduct = [
     productSleveLength: "Full Sleves",
     Browse: "Online Only",
     productpromotions: "Sale",
-    productDiscount: "60% above",
+    productDiscount: "60",
   },
   {
     proudimg:
@@ -93,7 +93,7 @@ var dataOfProduct = [
     productSleveLength: "Full Sleves",
     Browse: "Online Only",
     productpromotions: "Max300",
-    productDiscount: "60% above",
+    productDiscount: "60",
   },
   {
     proudimg:
@@ -106,7 +106,74 @@ var dataOfProduct = [
     productSleveLength: "Full Sleves",
     Browse: "Online Only",
     productpromotions: "Max300",
-    productDiscount: "20% above",
+    productDiscount: "20",
+  },
+
+  {
+    proudimg:
+      "https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010682836-Blue-Lightblue-1000010682836-9222021_01-2100.jpg",
+    produPrice: "₹999",
+    prodDesc: "MAX Printed Round Neck Sweatshirt",
+    productcolor: "blue",
+    producttype: "Hoddie",
+    productdesgine: "Jacquard",
+    productSleveLength: "Full Sleves",
+    Browse: "Online Only",
+    productpromotions: "Max300",
+    productDiscount: "70",
+  },
+
+  {
+    proudimg:
+      "https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010773077-Green-Oliveg-1000010773077-10202021_01-2100.jpg",
+    produPrice: "₹449",
+    prodDesc: "MAX Printed Round Sweatshirt",
+    productcolor: "Green",
+    producttype: "Hoddie",
+    productdesgine: "Jacquard",
+    productSleveLength: "Full Sleves",
+    Browse: "Online Only",
+    productpromotions: "Max300",
+    productDiscount: "80",
+  },
+  {
+    proudimg:
+      "https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000009224156-Pink-Blush-1000009224156-10182020_01-2100.jpg",
+    produPrice: "₹450",
+    prodDesc: "MAX Printed Hooded Sweatshirt",
+    productcolor: "pink",
+    producttype: "Hoddie",
+    productdesgine: "Printed",
+    productSleveLength: "Full Sleves",
+    Browse: "sale",
+    productpromotions: "Max300",
+    productDiscount: "50",
+  },
+  {
+    proudimg:
+      "https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010605412-Purple-Purple-1000010605412-9222021_01-2100.jpg",
+    produPrice: "₹999",
+    prodDesc: "MAX Textured Hooded Zip Sweatshirt",
+    productcolor: "purple",
+    producttype: "Front Open",
+    productdesgine: "Solid",
+    productSleveLength: "Full Sleves",
+    Browse: "sale",
+    productpromotions: "Max300",
+    productDiscount: "60",
+  },
+  {
+    proudimg:
+      "https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010503370-Pink-Peachapct-1000010503370-11302021_02-2100.jpg",
+    produPrice: "₹999",
+    prodDesc: "MAX Embellished Round Neck Sweatshirt",
+    productcolor: "pink",
+    producttype: "hoodie",
+    productdesgine: "Solid",
+    productSleveLength: "Full Sleves",
+    Browse: "sale",
+    productpromotions: "sale",
+    productDiscount: "60",
   },
 ];
 var count = document.querySelector("#countProduct");
@@ -128,6 +195,9 @@ dataOfProduct.map(function (ele) {
   var prouddescription = document.createElement("p");
   prouddescription.setAttribute("id", "proddesc");
   prouddescription.textContent = ele.prodDesc;
+
+  //   var bottomdiv = document.createElement("div");
+  //   productDiv.setAttribute("id", "bottondiv");
 
   var productbutton = document.createElement("button");
   productbutton.setAttribute("id", "buttonproduct");
